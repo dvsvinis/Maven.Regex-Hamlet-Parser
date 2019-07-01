ef9e890b736cd2f6f34610e883b505ace3b27d2d
donnasvinis <dvsvinis@mac.com>
2019-07-01 14:27:04 -0400

    Tests for find and replace and find completed.

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -42,17 +42,20 @@ public class HamletParser {
         return hamletData;
     }
 
+
     public static String exactMatch(String nameToSearchFor, String nameToSubstitute, String textToSearch) {
         Pattern p = Pattern.compile(nameToSearchFor);
         Matcher m = p.matcher(textToSearch);
         return m.replaceAll(nameToSubstitute);
     }
 
+
     public static String allUpperCaseMatch(String nameToSearchFor, String nameToSubstitute, String textToSearch) {
         Pattern p = Pattern.compile(nameToSearchFor.toUpperCase());
         Matcher m = p.matcher(textToSearch);
         return m.replaceAll(nameToSubstitute.toUpperCase());
     }
+
 
     public static boolean findName(String nameToSearchFor, String textToSearch) {
         Pattern p = Pattern.compile(nameToSearchFor);
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -35,13 +35,49 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHoratioToTariq() {
+        //Given
+        String nameToSearchFor = "Horatio";
+        String nameToSubstitute = "Tariq";
+        String newHamlet = "";
+
+        //When
+        newHamlet = HamletParser.exactMatch(nameToSearchFor, nameToSubstitute, hamletText);
+        newHamlet = HamletParser.allUpperCaseMatch(nameToSearchFor, nameToSubstitute, newHamlet);
+
+        Boolean containsHoratio = HamletParser.findName(nameToSearchFor, newHamlet);
+        Boolean containsHORATIO = HamletParser.findName(nameToSearchFor.toUpperCase(), newHamlet);
+
+        //Then
+        assertFalse(containsHoratio);
+        assertFalse(containsHORATIO);
     }
+
 
     @Test
     public void testFindHoratio() {
+        //Given
+        String nameToSearchFor = "Horatio";
+
+        //When
+        Boolean containsHoratio = HamletParser.findName(nameToSearchFor, hamletText);
+        Boolean containsHORATIO = HamletParser.findName(nameToSearchFor.toUpperCase(), hamletText);
+
+        //Then
+        assertTrue(containsHoratio);
+        assertTrue(containsHORATIO);
     }
 
     @Test
     public void testFindHamlet() {
+        //Given
+        String nameToSearchFor = "Hamlet";
+
+        //When
+        Boolean containsHamlet = HamletParser.findName(nameToSearchFor, hamletText);
+        Boolean containsHAMLET = HamletParser.findName(nameToSearchFor.toUpperCase(), hamletText);
+
+        //Then
+        assertTrue(containsHamlet);
+        assertTrue(containsHAMLET);
     }
 }
